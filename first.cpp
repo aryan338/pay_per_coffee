8449b84ef29e0981157c1398377e4cda0d9f2112
aryan338 <56729578+aryan338@users.noreply.github.com>
2020-02-09 21:40:26 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -2,16 +2,23 @@
 #include <pichosha.h>
 using namespace std;
 class Trasaction
-	string Payer[10],Payee[10];
+	public:
+        string Payer[10],Payee[10];
 	int amount;
 
 class Block
-	string prev_hash, cur_hash;
+	public:
+        string prev_hash=0, cur_hash;
 
 int main()
 {
-	for(i=1;i<+5;i++)
+	Trasaction Block1[5];
+        Block first;
+        for(i=0;i<5;i++)
 	{
-		cout<<"Enter the 
-}
+                cout<<"\nEnter the payee id, recipient id and the amount\n";
+                cin>>Block1[i].Payer>>Block1[i].Payee>>Block1[i].amount;
+                first.curr_hash = SHA256(first.prev_hash+Block1[i].Payer+Block1[i].Payee+Block1[i].amount);
+        }
 
+}
