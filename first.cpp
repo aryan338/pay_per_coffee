dda4cfa886e698dd2cd7b799834622685465762a
Aryan Mundada <rnmundra@iitk.ac.in>
2020-02-27 00:42:55 +0530

    hashing

--- first.cpp
+++ first.cpp
@@ -14,10 +14,17 @@ public:
 int main()
 {
 	Block Block_1;
+	string dat=prev_hash;
         for(i=0;i<5;i++)
 	{
 	        cout<<"\nEnter the payee id, recipient id and the amount\n";
 	        cin>>Block_1.Entry[i].Payer>>Block_1.Entry[i].Payee>>Block_1.Entry[i].amount;
+		picosha2::hash256_hex_str(dat,dat);
+		dat=dat+Block_1.Entry[i].Payee;
+		picosha2::hash256_hex_str(dat,dat);
+		dat=dat+Block_1.Entry[i].amount;
+		picosha2::hash256_hex_str(dat,dat);
 	}
+	cur_hash=dat;
 }
 
