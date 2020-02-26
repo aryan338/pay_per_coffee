5f56aea702ceac4b83cacb2c4dc198ebd707e170
Aryan Mundada <rnmundra@iitk.ac.in>
2020-02-27 00:12:41 +0530

    Restore

---
+++ first.cpp
@@ -0,0 +1,23 @@
+#include <iotream>
+#include "pichosha.h"
+using namespace std;
+class Trasaction
+public:
+	string Payer,Payee;
+	int amount;
+
+class Block
+public:
+        string prev_hash, cur_hash;
+	Transaction Entry[5];
+
+int main()
+{
+	Block Block_1;
+        for(i=0;i<5;i++)
+	{
+	        cout<<"\nEnter the payee id, recipient id and the amount\n";
+	        cin>>Block_1.Entry[i].Payer>>Block_1.Entry[i].Payee>>Block_1.Entry[i].amount;
+	}
+}
+
